6a9b0b4ac68c8cc60c823314e976d344f7af5d25
Zheng Jianing <jianing.zheng@rea-group.com>
2013-10-28 22:05:48 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xiaozhan::Application.config.secret_key_base = '98be0bff9d9821cfee4ec8e313613bb185212a5aae5eb6094bc9e0c55b2e69ced2bf073b3657924a8d57c1a0a12a7426c83a6684876f617bbef479984e9cfc4c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Xiaozhan::Application.config.session_store :cookie_store, key: '_xiaozhan_session'
